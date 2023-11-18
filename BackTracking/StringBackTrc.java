eed6aa1b997548c9cc0bd1c4e0347924c666a88c
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-11-19 02:54:05 +0530

    update data

---
+++ BackTracking/StringBackTrc.java
@@ -0,0 +1,23 @@
+package BackTracking;
+
+public class StringBackTrc {
+    public static void main(String[] args) {
+        String str = "ABC";
+        printPermutation(str, "", 0);
+
+    }
+
+    static void printPermutation(String str, String perm, int index) {
+        if (str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(newStr, perm + currentChar, index + 1);
+        }
+
+    }
+}
--- JavaCon.java
+++ JavaCon.java
@@ -2,14 +2,8 @@ public class JavaCon {
     public static void main(String[] args) {
     
         nameChanged(null);
-        StringBuffer sBuffer = new StringBuffer("Geeka");
-        String vlString = sBuffer.toString();           
-        System.out.println(vlString);
-
-        StringBuilder sBuilder = new StringBuilder();
-        sBuilder.append("vlString");
-        System.out.println(sBuilder);
-
+       
+    
     }
     
     public static void nameChanged(String printString){
